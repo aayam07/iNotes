c1290b1ec7cd5e1e748d28922405bd61111886b0
Aayam Adhikari <thisisleo7@gmail.com>
2023-09-18 19:12:54 +0545

    Understand Core Data template and comment imp notes for functionalities.

--- iNotes/Model/Persistence.swift
+++ iNotes/Model/Persistence.swift
@@ -8,11 +8,41 @@
 import CoreData
 
 struct PersistenceController {
+    //MARK: - 1. PERSISTENT CONTROLLER.
+    // Sets up the model, content, and store co-ordinator all at once
     static let shared = PersistenceController()
 
+    //MARK: - 2. PERSISTENT CONTAINER
+    // STORAGE PROPERTY FOR CORE DATA. PREFERRED WAY TO INITIALIZE OUR CORE DATA STACK,
+    // AND LOAD OUR CORE DATA MODEL
+    let container: NSPersistentContainer
+
+    //MARK: - 3. INITIALIZE THE CORE DATA STACK (then load the peristent store)
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "iNotes") // refering the data model
+        
+        // path to the temporary storage (we can access the non-disk storage, and is
+        // great for unit testing and for using in the SwiftUI preview)
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        
+        // load the store (persistent or temporary)
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+    
+    //MARK: - 4. PREVIEW (test configuration for the SwiftUI previews)
     static var preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
+        let result = PersistenceController(inMemory: true) // switch from default disk based data store to inMemory store
         let viewContext = result.container.viewContext
+        
+        // set up sample data to show in the preview (i.e in simulator)
         for _ in 0..<10 {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
@@ -20,37 +50,10 @@ struct PersistenceController {
         do {
             try viewContext.save()
         } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            
             let nsError = error as NSError
             fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
         return result
     }()
-
-    let container: NSPersistentContainer
-
-    init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "iNotes")
-        if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
-        }
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        container.viewContext.automaticallyMergesChangesFromParent = true
-    }
 }
---
+++ iNotes/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  iNotes
+//
+//  Created by Aayam Adhikari on 18/09/2023.
+//
+
+import SwiftUI
+
+//MARK: - FORMATTER
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+//MARK: - UI
+
+//MARK: - UX
